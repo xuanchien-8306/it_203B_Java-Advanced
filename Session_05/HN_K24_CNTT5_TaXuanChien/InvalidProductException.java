ede20a21713ab889257fc660353db08760c8b138
xuanchien-8306 <xuanchien319@gmail.com>
2026-03-12 23:29:50 +0700

    Delete Session_05 directory

--- Session_05/HN_K24_CNTT5_TaXuanChien/InvalidProductException.java
+++
@@ -1,9 +0,0 @@
-package Session_05.HN_K24_CNTT5_TaXuanChien;
-
-public class InvalidProductException extends Exception {
-
-    public InvalidProductException(String message) {
-        super(message);
-    }
-
-}
